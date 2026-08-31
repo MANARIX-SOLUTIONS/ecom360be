513622f93cee006dff65434c063e64cd146d2f03
Saïfoulaye Diallo <saifoulaye.diallo0512@gmail.com>
2026-08-31 22:56:01 +0000

    feat: Remove deprecated DTOs and services related to commerce integration

--- src/main/java/com/ecom360/integration/commerce/application/dto/CommerceConnectionUpdateRequest.java
+++
@@ -1,6 +0,0 @@
-package com.ecom360.integration.commerce.application.dto;
-
-import jakarta.validation.constraints.NotNull;
-
-public record CommerceConnectionUpdateRequest(@NotNull Boolean isActive) {
-}
